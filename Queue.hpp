cd48afd6210a74356893bd569bdab10d9c4755fd
AmirGill <amirg00@walla.com>
2022-04-20 11:50:47 +0300

    Upload stack header

--- Queue.hpp
+++ Queue.hpp
@@ -1,1 +1,13 @@
-//////////////
+#include <vector>
+using std::string;
+
+class Stack{
+private:
+    vector<string> _data{};
+public:
+    Stack(int capacity);
+    void PUSH(const string& text);
+    string POP();
+    void TOP() const;
+    bool isEmpty() const;
+};
