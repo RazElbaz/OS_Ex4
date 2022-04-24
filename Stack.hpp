a30291e6f21c2cb211cd539a12a6b894ec9714fa
AmirGill <amirg00@walla.com>
2022-04-24 16:15:52 +0300

    Change stack header to use pure c dynamic implementation

--- Stack.hpp
+++ Stack.hpp
@@ -1,16 +1,26 @@
-#include <string>
+#include <string.h>
 #include <iostream>
-#include <vector>
-using std::string;
 
-class Stack{
-private:
-    std::vector<string> _data{};
-public:
-    void PUSH(const string& text);
-    string POP();
-    string TOP() const;
-    bool isEmpty() const;
-    void clear();
-    void print_stack();
+#define MAX_TEXT_SIZE 1024 //BYTES
+
+typedef struct Node{
+    char data[MAX_TEXT_SIZE];
+    struct Node *next;
+} node, *node_ptr;
+
+struct Stack{
+    int size;
+    node_ptr head;
 };
+
+//*****************************/
+//========== Methods ==========/
+//*****************************/
+
+Stack* create();
+void PUSH(Stack* stack, const char* text);
+char* POP(Stack* stack, int *err_flag);
+char* TOP(Stack* stack, int *err_flag);
+bool isEmpty(Stack* stack);
+void clear(Stack* stack);
+void print_stack(Stack* stack);
